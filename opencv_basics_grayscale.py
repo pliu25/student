0da76f94f4a4f5ea6d6f078de36831915ab462a0
pliu25 <pearl.liu25@trinityschoolnyc.org>
2024-05-10 00:36:11 -0400

    update

--- opencv_basics_grayscale.py
+++ opencv_basics_grayscale.py
@@ -84,16 +84,41 @@ def image_average_grayscale(GS_image1:list, GS_image2:list)-> list:
 
     """
     # WRITE YOUR CODE HERE.
-    pixel_values = []
+    pixel_values1 = []
+    pixel_values2 = []
 
-    # for list in GS_image1:
-    #     for list2 in GS_image2:
-    #         for pixel in list:
-    #             for pixel2 in list2:
-    #                 #pixel_values.append(math.trunc((pixel + pixel2)/2))
-    #                 print(list)
-    # image3 = pixel_values 
-    return GS_image1
+    img1 = GS_image1.copy()
+    img2 = GS_image2.copy()
+    img = GS_image1.copy()
+
+    list1 = []
+    list2 = []
+    #for r in zip(img1, img2):
+        #sum = sum(r)
+        #print(r)
+        # for c in zip(img1[r], img2[r]):
+        #     img[r][c] = math.trunc(sum(c) / 2)
+        # for c in enumerate(zip(img1[r], img2[r])):
+        #     img[r][c] = (img1[r][c] + img2[r][c])/2
+    
+    # for r, row in enumerate(img1):
+    #     list1.append(row)
+    # for r, row in enumerate(img2):
+    #     list2.append(row)
+    # print(list1)
+    # for r in range(len(list1)):
+    #     for c in range(len(r)):
+    #         img[r][c] = (list1[c] + list2[c])/2
+
+    for r, row in enumerate(img):
+        for c, (value1, value2) in enumerate(zip(row, img2[r])):
+            img[r][c] = math.trunc((value1 + value2)/2)
+
+            
+    # print(img1)
+    # print(img2)
+    # print(img)
+    return img
     # END OF FUNCTION.
 
 def flip_horizontal_grayscale(GS_image:list)->list:
@@ -143,9 +168,10 @@ def histogram_grayscale(GS_image:list)->list:
         if value not in pixels_dict:
             pixels_dict[value] = pixel_list.count(value)
     value_list = sorted(pixels_dict.keys())
+    print(pixels_dict)
     for value in value_list:
         value_list[value] = pixels_dict[value_list[value]]
-    #print(value_list)
+    print(len(value_list))
     return value_list
     # END OF FUNCTION.
 
